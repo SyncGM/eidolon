3156fe477bb19b79a1e04dac7a6fe0e7bc35427e
Rachel Wall <solistra@gmx.com>
2014-04-22 22:09:10 -0400

    Remove unnecessary `self` return in `Table` RGSSx class.

--- lib/eidolon/rgssx/table.rb
+++ lib/eidolon/rgssx/table.rb
@@ -6,7 +6,6 @@ class Table
   def self._load(array) # :nodoc:
     self.class.new.instance_eval do
       @size, @xsize, @ysize, @zsize, _, *@data = array.unpack('LLLLLS*')
-      self
     end
   end
 end
